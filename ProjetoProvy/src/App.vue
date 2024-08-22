70181941e100ec010d42309de34e5f1fcf373134
CesarSoldier <brennoemmet@gmail.com>
2024-08-22 11:16:50 -0400

    22/08 o começo

---
+++ ProjetoProvy/src/App.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <div id="app">
+    <h1>Bem vindo ao meu projeto Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'App',
+};
+</script>
+
+<style>
+  /* estilos globais */
+</style>
