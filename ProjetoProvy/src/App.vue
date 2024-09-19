098f9333a0e28f3527cdd7f693e31dfcebfd5a20
CesarSoldier <brennoemmet@gmail.com>
2024-09-18 21:39:19 -0400

    header atualização

--- ProjetoProvy/src/App.vue
+++ ProjetoProvy/src/App.vue
@@ -2,26 +2,6 @@
   <div id="app">
     <header-component />
 
-    <header>
-            <h1>Bem-vindo à Provy</h1>
-            <p>Sua plataforma de prestação de serviços.</p>
-        </header>
-
-    <div class="navigation-buttons">
-      <router-link to="/cadastrocliente">
-        <button class="btn">Cadastro de Cliente</button>
-      </router-link>
-      <router-link to="/cadastroprestador">
-        <button class="btn">Cadastro de Prestador de Serviço</button>
-      </router-link>
-      <p>Já possui uma conta?</p> 
-      <div class="button-login">
-     <router-link to="/login"> 
-      <button class="btn">Faça o login</button>
-    </router-link>
-     </div>
-    </div>
-
     <main class="main-content">
       <div v-if="$route.path === '/'">
         <service-list />
